3ea1075740702c2a98ef593ec12cb6a56c1aba96
limjonghyeok <jjognbbang2@naver.com>
2024-06-05 16:13:17 +0900

    init

---
+++ SchoolSafetyCrab/src/test/java/com/schoolsafetycrab/SchoolSafetyCrabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolsafetycrab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSafetyCrabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
