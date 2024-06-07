351a4bd55486f0ca62aad27b1ae1fef853fee760
limjonghyeok <jjognbbang2@naver.com>
2024-06-07 23:59:13 +0900

    jwt 생성

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/global/exception/CustomException.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/global/exception/CustomException.java
@@ -10,6 +10,10 @@ public enum CustomException {
     //400
     NOT_EMPTY_ROLE_EXCEPTION(400,"NotEmptyRoleException","권한이 존재하지 않습니다."),
     NOT_FOUND_USER_EXCEPTION(400,"NotFoundUserException","유저가 존재하지 않습니다."),
+
+    //인증 에러 401
+    EXPIRED_JWT_EXCEPTION(401,"ExpiredJwtException","토큰이 만료했습니다."),
+    NOT_VALID_JWT_EXCEPTION(401,"NotValidJwtException","토큰이 유효하지 않습니다."),
     ;
 
     private int statusNum;
