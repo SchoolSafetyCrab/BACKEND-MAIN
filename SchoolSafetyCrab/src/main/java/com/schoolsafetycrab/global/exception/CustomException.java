c6a571d52bb3ec4553e18257f5ec1406223edb3c
limjonghyeok <jjognbbang2@naver.com>
2024-06-08 00:26:30 +0900

    로그인시 id password 를 통한 UsernamePasswordAuthenticationToken 생성

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/global/exception/CustomException.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/global/exception/CustomException.java
@@ -11,6 +11,8 @@ public enum CustomException {
     NOT_EMPTY_ROLE_EXCEPTION(400,"NotEmptyRoleException","권한이 존재하지 않습니다."),
     NOT_FOUND_USER_EXCEPTION(400,"NotFoundUserException","유저가 존재하지 않습니다."),
 
+    ID_PASSWORD_INPUT_EXCEPTION(400,"IdPasswordInputException", "아이디 패스워드 입력이 잘못 되었습니다."),
+
     //인증 에러 401
     EXPIRED_JWT_EXCEPTION(401,"ExpiredJwtException","토큰이 만료했습니다."),
     NOT_VALID_JWT_EXCEPTION(401,"NotValidJwtException","토큰이 유효하지 않습니다."),
---
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/global/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,38 @@
+package com.schoolsafetycrab.global.security.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.schoolsafetycrab.domain.user.model.User;
+import com.schoolsafetycrab.global.exception.CustomException;
+import com.schoolsafetycrab.global.exception.ExceptionResponse;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try{
+            User user = objectMapper.readValue(request.getInputStream(), User.class);
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getId(),user.getPassword());
+            return getAuthenticationManager().authenticate(authenticationToken);
+        }catch (IOException e){
+            log.error(e.getMessage());
+            throw new ExceptionResponse(CustomException.ID_PASSWORD_INPUT_EXCEPTION);
+        }
+    }
+
+}
