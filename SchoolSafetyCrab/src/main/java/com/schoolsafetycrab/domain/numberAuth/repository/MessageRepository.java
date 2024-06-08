583064ecc73a5eb6585dc9d6a8773a401078a792
limjonghyeok <jjognbbang2@naver.com>
2024-06-08 12:39:30 +0900

    인증 번호 전송 컨트롤러 테스트

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/numberAuth/repository/MessageRepository.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/numberAuth/repository/MessageRepository.java
@@ -2,6 +2,7 @@ package com.schoolsafetycrab.domain.numberAuth.repository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
@@ -13,10 +14,10 @@ import java.time.Duration;
 @Slf4j
 public class MessageRepository {
 
-    private final RedisTemplate redisTemplate;
+    private final RedisTemplate<Object, Object> redisTemplate;
 
     public void saveAuth(String phoneNumber, Object authCode){
-        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
-        valueOperations.set(phoneNumber,authCode, Duration.ofMinutes(5));
+        ValueOperations<Object, Object> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(phoneNumber, authCode, Duration.ofMinutes(5));
     }
 }
