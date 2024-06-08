e2a14cefbb989b605c2bbb746e66e10375d7a9d8
limjonghyeok <jjognbbang2@naver.com>
2024-06-08 14:42:26 +0900

    인증 번호 검증 실패 테스트

--- SchoolSafetyCrab/src/test/java/com/schoolsafetycrab/domain/message/service/MessageServiceCheckTest.java
+++ SchoolSafetyCrab/src/test/java/com/schoolsafetycrab/domain/message/service/MessageServiceCheckTest.java
@@ -3,6 +3,8 @@ package com.schoolsafetycrab.domain.message.service;
 import com.schoolsafetycrab.domain.numberAuth.repository.MessageRepository;
 import com.schoolsafetycrab.domain.numberAuth.requestDto.CheckAuthCodeRequestDto;
 import com.schoolsafetycrab.domain.numberAuth.service.NumberAuthService;
+import com.schoolsafetycrab.global.exception.CustomException;
+import com.schoolsafetycrab.global.exception.ExceptionResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -39,6 +41,19 @@ public class MessageServiceCheckTest {
         Assertions.assertThatNoException().isThrownBy(() -> numberAuthService.checkAuthCode(requestDto));
 
         //then
+        BDDMockito.then(messageRepository).should().checkAuth(requestDto.getPhoneNumber(), requestDto.getAuthCode());
+    }
+
+    @Test
+    @DisplayName("인증번호 검증 실패 테스트")
+    public void 인증번호_검증_실패_테스트(){
+        //given
+        BDDMockito.given(messageRepository.checkAuth(requestDto.getPhoneNumber(), requestDto.getAuthCode())).willReturn(false);
+
+        Assertions.assertThatThrownBy(() -> numberAuthService.checkAuthCode(requestDto))
+                .isInstanceOf(ExceptionResponse.class)
+                .hasFieldOrPropertyWithValue("customException",CustomException.NOT_MATCH_AUTH_CODE);
+
         BDDMockito.then(messageRepository).should().checkAuth(requestDto.getPhoneNumber(), requestDto.getAuthCode());
     }
 }
