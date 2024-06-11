58468e747d8088dca3865b0956e01d55988a6b85
limjonghyeok <jjognbbang2@naver.com>
2024-06-12 00:55:33 +0900

    cors 설정

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/numberAuth/requestDto/CheckAuthCodeRequestDto.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/numberAuth/requestDto/CheckAuthCodeRequestDto.java
@@ -1,9 +1,10 @@
 package com.schoolsafetycrab.domain.numberAuth.requestDto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @AllArgsConstructor
