4941c1a07de9643fb37a229fa268658da515d7bb
limjonghyeok <jjognbbang2@naver.com>
2024-06-13 08:53:51 +0900

    부모 지정 서비스 설정

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/user/service/StudentService.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/user/service/StudentService.java
@@ -1,5 +1,45 @@
 package com.schoolsafetycrab.domain.user.service;
 
+import com.schoolsafetycrab.domain.guardian.model.Guardian;
+import com.schoolsafetycrab.domain.guardian.repository.GuardianRepository;
+import com.schoolsafetycrab.domain.user.model.Role;
+import com.schoolsafetycrab.domain.user.model.User;
+import com.schoolsafetycrab.domain.user.repository.UserRepository;
+import com.schoolsafetycrab.domain.user.requestDto.DesignateGuardianRequestDto;
+import com.schoolsafetycrab.global.exception.CustomException;
+import com.schoolsafetycrab.global.exception.ExceptionResponse;
+import com.schoolsafetycrab.global.security.auth.PrincipalDetails;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
 public class StudentService {
+
+    private final UserRepository userRepository;
+    private final GuardianRepository guardianRepository;
+
+    @Transactional
+    public void designateGuardian(Authentication authentication, DesignateGuardianRequestDto requestDto){
+        User user = ((PrincipalDetails) authentication.getPrincipal()).getUser();
+        User guardian = userRepository.findUserByIdAndState(requestDto.getGuardianId(),true).orElseThrow(() -> {
+            throw new ExceptionResponse(CustomException.NOT_FOUND_USER_EXCEPTION);
+        });
+
+        if(guardian.getRole().equals(Role.ROLE_STUDENT)){
+            throw new ExceptionResponse(CustomException.DENIEND_STUDENT_EXCEPTION);
+        }
+
+        if(guardianRepository.existsGuardianByUserAndId(user,guardian.getId())){
+            throw new ExceptionResponse(CustomException.DUPLICATED_GUARDIAN_ID_EXCEPTION);
+        }
+
+        Guardian designateGuardian = Guardian.createGuardian(user,guardian.getId());
+        guardianRepository.save(designateGuardian);
+    }
 }
--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/global/exception/CustomException.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/global/exception/CustomException.java
@@ -14,10 +14,14 @@ public enum CustomException {
     NOT_MATCH_AUTH_CODE(400,"NotMatchAuthCode","인증번호가 일치하지 않습니다."),
     DUPLICATED_NUMBER_EXCEPTION(400,"DuplicatedNumberException","가입된 전화번호가 존재합니다."),
     DUPLICATED_ID_EXCEPTION(400,"DuplicatedIDException","가입된 아이디가 존재합니다."),
+    DUPLICATED_GUARDIAN_ID_EXCEPTION(400,"DuplicatedGuardianIDException","해당 보호자가 존재합니다."),
 
     //인증 에러 401
     EXPIRED_JWT_EXCEPTION(401,"ExpiredJwtException","토큰이 만료했습니다."),
     NOT_VALID_JWT_EXCEPTION(401,"NotValidJwtException","토큰이 유효하지 않습니다."),
+
+    //403
+    DENIEND_STUDENT_EXCEPTION(403,"DeniendStudentException","학생은 권한이 없습니다")
     ;
 
     private int statusNum;
