ea18e3b2c1ea9222ff4898397547a829d7af4998
limjonghyeok <jjognbbang2@naver.com>
2024-06-13 08:41:17 +0900

    학생 보호자 지정 url 설정

---
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/user/requestDto/DesignateGuardianRequestDto.java
@@ -0,0 +1,15 @@
+package com.schoolsafetycrab.domain.user.requestDto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DesignateGuardianRequestDto {
+
+    @NotBlank
+    private String guardianId;
+}
