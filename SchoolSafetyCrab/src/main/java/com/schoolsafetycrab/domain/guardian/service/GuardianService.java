7fc39387cf361babd936e849137118c2324f8855
limjonghyeok <jjognbbang2@naver.com>
2024-06-13 18:52:13 +0900

    보호자 자식 가져오기 service

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/guardian/service/GuardianService.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/guardian/service/GuardianService.java
@@ -1,5 +1,6 @@
 package com.schoolsafetycrab.domain.guardian.service;
 
+import com.schoolsafetycrab.domain.guardian.message.responseDto.MyChildrenResponseDto;
 import com.schoolsafetycrab.domain.guardian.repository.GuardianRepository;
 import com.schoolsafetycrab.domain.user.model.User;
 import com.schoolsafetycrab.global.security.auth.PrincipalDetails;
@@ -9,6 +10,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,10 +21,18 @@ public class GuardianService {
 
     private final GuardianRepository guardianRepository;
 
-    public List<User> myChildren(Authentication authentication){
+    public List<MyChildrenResponseDto> myChildren(Authentication authentication){
         User user = ((PrincipalDetails)authentication.getPrincipal()).getUser();
         List<User> children = guardianRepository.findByMyChildren(user.getId());
-        return children;
+
+        List<MyChildrenResponseDto> responses = new ArrayList<>();
+
+        for(User child : children){
+            MyChildrenResponseDto response = MyChildrenResponseDto.userToMyChildrenResponseDto(child);
+            responses.add(response);
+        }
+
+        return responses;
     }
 
 }
