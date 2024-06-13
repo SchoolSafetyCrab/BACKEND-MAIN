4ed0bcb2760cd11842ef64b5d5c137d51101e29b
limjonghyeok <jjognbbang2@naver.com>
2024-06-13 18:54:06 +0900

    보호자 자식 가져오기 controller

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/guardian/controller/GuardianController.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/guardian/controller/GuardianController.java
@@ -1,6 +1,8 @@
 package com.schoolsafetycrab.domain.guardian.controller;
 
+import com.schoolsafetycrab.domain.guardian.message.responseDto.MyChildrenResponseDto;
 import com.schoolsafetycrab.domain.guardian.service.GuardianService;
+import com.schoolsafetycrab.global.util.HttpResponseUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -8,16 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/parents")
 public class GuardianController {
 
     private final GuardianService guardianService;
+    private final HttpResponseUtil responseUtil;
 
     @GetMapping
     public ResponseEntity<?> myChildren(Authentication authentication){
-
+        List<MyChildrenResponseDto> responses = guardianService.myChildren(authentication);
+        ResponseEntity<Map<String,Object>> response = responseUtil.createResponse(responses);
+        return response;
     }
 
 }
