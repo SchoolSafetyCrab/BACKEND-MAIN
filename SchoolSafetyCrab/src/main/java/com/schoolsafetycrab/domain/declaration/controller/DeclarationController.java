95d80db5c3bd203451b71f2abfa21e3b62ec2e03
limjonghyeok <jjognbbang2@naver.com>
2024-06-14 08:59:28 +0900

    신고 controller

---
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/declaration/controller/DeclarationController.java
@@ -0,0 +1,12 @@
+package com.schoolsafetycrab.domain.declaration.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/declaration")
+public class DeclarationController {
+
+}
