85a782d41415261b56897b73e3050755710ad608
limjonghyeok <jjognbbang2@naver.com>
2024-06-13 10:05:53 +0900

    부모 지정 성공 테스트

---
+++ SchoolSafetyCrab/src/test/java/com/schoolsafetycrab/domain/user/service/UserStudentGuardianTest.java
@@ -0,0 +1,67 @@
+package com.schoolsafetycrab.domain.user.service;
+
+import com.schoolsafetycrab.domain.guardian.repository.GuardianRepository;
+import com.schoolsafetycrab.domain.user.model.Role;
+import com.schoolsafetycrab.domain.user.model.User;
+import com.schoolsafetycrab.domain.user.repository.UserRepository;
+import com.schoolsafetycrab.domain.user.requestDto.DesignateGuardianRequestDto;
+import com.schoolsafetycrab.global.security.auth.PrincipalDetails;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+public class UserStudentGuardianTest {
+
+    @InjectMocks
+    private StudentService studentService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private GuardianRepository guardianRepository;
+
+    @Mock
+    private Authentication authentication;
+    @Mock
+    private PrincipalDetails principalDetails;
+
+    private User user;
+    private User guardian;
+    private DesignateGuardianRequestDto requestDto;
+
+    @BeforeEach
+    public void init(){
+        user = User.createUser("test","test","test","test", Role.ROLE_STUDENT,"010-1111-1111");
+        guardian = User.createUser("test1","test","test","test", Role.ROLE_PARENTS,"010-1111-1112");
+        requestDto = new DesignateGuardianRequestDto("test1");
+    }
+    @Test
+    @DisplayName("부모 지정 테스트")
+    public void 부모_지정_성공_테스트(){
+        //given
+        BDDMockito.given(userRepository.findUserByIdAndState(guardian.getId(),true)).willReturn(Optional.ofNullable(guardian));
+        BDDMockito.given(guardianRepository.existsGuardianByUserAndId(user,guardian.getId())).willReturn(false);
+        BDDMockito.given(authentication.getPrincipal()).willReturn(principalDetails);
+        BDDMockito.given(principalDetails.getUser()).willReturn(user);
+
+        //when
+        Assertions.assertThatNoException().isThrownBy(() -> studentService.designateGuardian(authentication,requestDto));
+
+        //then
+        BDDMockito.then(userRepository).should().findUserByIdAndState(guardian.getId(),true);
+        BDDMockito.then(guardianRepository).should().existsGuardianByUserAndId(user,guardian.getId());
+    }
+}
