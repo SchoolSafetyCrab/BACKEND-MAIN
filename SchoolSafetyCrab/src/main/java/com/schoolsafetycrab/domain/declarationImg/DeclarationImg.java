eb1383c92ffa18b8e96a90c0d8cdaa31fcc96cec
limjonghyeok <jjognbbang2@naver.com>
2024-06-14 15:37:39 +0900

    신고 이미지 엔티티 생성

---
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/declarationImg/DeclarationImg.java
@@ -0,0 +1,35 @@
+package com.schoolsafetycrab.domain.declarationImg;
+
+import com.schoolsafetycrab.domain.declaration.model.Declaration;
+import com.schoolsafetycrab.global.baseTimeEntity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class DeclarationImg extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "declaration_img_id")
+    private String declarationImgId;
+
+    @ManyToOne
+    @JoinColumn(name = "declaration_id")
+    private Declaration declaration;
+
+    @Column(name = "img_url", nullable = false)
+    private String imgUrl;
+
+    private DeclarationImg(Declaration declaration, String imgUrl) {
+        this.declaration = declaration;
+        this.imgUrl = imgUrl;
+    }
+
+    public DeclarationImg createDeclarationImg(Declaration declaration, String img){
+        return new DeclarationImg(declaration,imgUrl);
+    }
+}
