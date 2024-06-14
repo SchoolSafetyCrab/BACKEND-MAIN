348f4d1809e2ab757e5022174bae2756997c8f70
limjonghyeok <jjognbbang2@naver.com>
2024-06-14 13:04:19 +0900

    신고 성공 컨트롤러 테스트

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/declaration/message/SuccessMessage.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/declaration/message/SuccessMessage.java
@@ -1,10 +1,11 @@
 package com.schoolsafetycrab.domain.declaration.message;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public enum SuccessMessage {
 
     SUCCESS_REQUEST_DECLARATION("신고 요청 성공")
--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/user/message/SuccessStudentMessage.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/user/message/SuccessStudentMessage.java
@@ -1,10 +1,11 @@
 package com.schoolsafetycrab.domain.user.message;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public enum SuccessStudentMessage {
 
     SUCCESS_DESIGNATE_GUARDIAN("보호자 지정 성공")
