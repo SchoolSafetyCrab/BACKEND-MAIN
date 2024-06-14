41365b94a639fee907d067604bc41b85904d8e19
limjonghyeok <jjognbbang2@naver.com>
2024-06-14 12:07:05 +0900

    신고 service 신고 저장 생성

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/declaration/controller/DeclarationController.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/declaration/controller/DeclarationController.java
@@ -1,6 +1,11 @@
 package com.schoolsafetycrab.domain.declaration.controller;
 
+import com.schoolsafetycrab.domain.declaration.requestDto.DeclarationRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/declaration")
 public class DeclarationController {
+
+    @PostMapping
+    public ResponseEntity<?> requestDeclaration(Authentication authentication, @RequestBody DeclarationRequestDto requestDto){
+
+    }
 
 }
