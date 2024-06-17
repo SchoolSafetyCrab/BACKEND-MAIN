269851df2f82209ab6a19ff858636650729c2808
honggyeonghyeon <hyeon21owo@gmail.com>
2024-06-17 22:09:36 +0900

    Schoolway controller 생성

---
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/schoolway/controller/SchoolWayController.java
@@ -0,0 +1,28 @@
+package com.schoolsafetycrab.domain.schoolway.controller;
+
+import com.schoolsafetycrab.domain.schoolway.service.SchoolWayService;
+import com.schoolsafetycrab.global.util.HttpResponseUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/student")
+public class SchoolWayController {
+
+    private final SchoolWayService schoolWayService;
+    private final HttpResponseUtil responseUtil;
+
+    @PostMapping("/save/schoolway")
+    public ResponseEntity<?> saveSchoolWay(Authentication authentication) {
+        schoolWayService.saveSchoolWay(authentication);
+        ResponseEntity<Map<String, Object>> response = responseUtil.createResponse();
+    }
+
+}
