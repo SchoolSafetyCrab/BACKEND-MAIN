a9bf3fab96262128f2ec1df5dab20737d2390da8
honggyeonghyeon <hyeon21owo@gmail.com>
2024-06-17 22:45:33 +0900

    Schoolway controller save request body 추가

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/schoolway/controller/SchoolWayController.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/schoolway/controller/SchoolWayController.java
@@ -1,12 +1,15 @@
 package com.schoolsafetycrab.domain.schoolway.controller;
 
 import com.schoolsafetycrab.domain.schoolway.message.SuccessSchoolWayMessage;
+import com.schoolsafetycrab.domain.schoolway.requestDto.SchoolWayPointRequestDto;
 import com.schoolsafetycrab.domain.schoolway.service.SchoolWayService;
 import com.schoolsafetycrab.global.util.HttpResponseUtil;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +24,8 @@ public class SchoolWayController {
     private final HttpResponseUtil responseUtil;
 
     @PostMapping("/save/schoolway")
-    public ResponseEntity<?> saveSchoolWay(Authentication authentication) {
-        schoolWayService.saveSchoolWay(authentication);
+    public ResponseEntity<?> saveSchoolWay(Authentication authentication, @Valid @RequestBody SchoolWayPointRequestDto requestDto) {
+        schoolWayService.saveSchoolWay(authentication, requestDto);
         ResponseEntity<Map<String, Object>> response = responseUtil.createResponse(SuccessSchoolWayMessage.SUCCESS_SCHOOL_WAY);
         return response;
     }
