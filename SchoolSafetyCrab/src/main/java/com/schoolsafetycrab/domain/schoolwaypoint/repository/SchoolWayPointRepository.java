72e2aa76d6c1050bf9d261811f2f5e610d3b7695
honggyeonghyeon <hyeon21owo@gmail.com>
2024-06-17 22:35:48 +0900

    SchoolwayPoint Repository 생성

---
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/schoolwaypoint/repository/SchoolWayPointRepository.java
@@ -0,0 +1,10 @@
+package com.schoolsafetycrab.domain.schoolwaypoint.repository;
+
+import com.schoolsafetycrab.domain.schoolwaypoint.model.SchoolWayPoint;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SchoolWayPointRepository extends JpaRepository<SchoolWayPoint, Long> {
+
+}
