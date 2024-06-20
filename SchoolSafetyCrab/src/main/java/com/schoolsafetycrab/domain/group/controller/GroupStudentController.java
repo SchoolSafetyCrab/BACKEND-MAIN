a7b42c576fd25e1973d75bfaf459bf53ce38c658
jaehee Jung <wjfwogml5890@naver.com>
2024-06-21 04:54:50 +0900

    group message 및 controller 작성

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/group/controller/GroupStudentController.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/group/controller/GroupStudentController.java
@@ -1,9 +1,11 @@
 package com.schoolsafetycrab.domain.group.controller;
 
 import com.schoolsafetycrab.domain.group.message.SuccessGroupMessage;
+import com.schoolsafetycrab.domain.group.requestDto.RegistGroupRequestDto;
 import com.schoolsafetycrab.domain.group.service.GroupService;
 import com.schoolsafetycrab.domain.group.requestDto.CreateGroupRequestDto;
 import com.schoolsafetycrab.global.util.HttpResponseUtil;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -21,4 +23,12 @@ public class GroupStudentController {
 
     private final GroupService groupService;
     private final HttpResponseUtil responseUtil;
+
+    @PostMapping("/regist")
+    public ResponseEntity<?> registGroup(Authentication authentication, @Valid @RequestBody RegistGroupRequestDto registGroupRequestDto) {
+        groupService.registGroup(authentication, registGroupRequestDto);
+        ResponseEntity<Map<String, Object>> response = responseUtil.createResponse(SuccessGroupMessage.SUCCESS_REGIST_GROUP);
+
+        return response;
+    }
 }
