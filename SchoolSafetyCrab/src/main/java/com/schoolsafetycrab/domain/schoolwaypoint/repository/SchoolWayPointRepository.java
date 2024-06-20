cb7ab917d38afcf1e72bcfbeeefe321ac7a3ca14
honggyeonghyeon <hyeon21owo@gmail.com>
2024-06-21 01:51:15 +0900

    SchoolWayPointRepository extends custom

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/schoolwaypoint/repository/SchoolWayPointRepository.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/schoolwaypoint/repository/SchoolWayPointRepository.java
@@ -1,12 +1,19 @@
 package com.schoolsafetycrab.domain.schoolwaypoint.repository;
 
+import com.schoolsafetycrab.domain.schoolway.message.responseDto.PointResponseDto;
 import com.schoolsafetycrab.domain.schoolway.model.SchoolWay;
 import com.schoolsafetycrab.domain.schoolwaypoint.model.SchoolWayPoint;
+import com.schoolsafetycrab.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface SchoolWayPointRepository extends JpaRepository<SchoolWayPoint, Long> {
+public interface SchoolWayPointRepository extends JpaRepository<SchoolWayPoint, Long>, SchoolWayPointRepositoryCustom {
 
     void deleteAllBySchoolWay(SchoolWay schoolWay);
+
+    @Override
+    List<PointResponseDto> findByUser(User user);
 }
