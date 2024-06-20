57d893bef8951d2f39302b0ece18ad560957d79f
honggyeonghyeon <hyeon21owo@gmail.com>
2024-06-20 21:05:15 +0900

    SchoolWayMessage delete 추가

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/schoolway/message/SuccessSchoolWayMessage.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/schoolway/message/SuccessSchoolWayMessage.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public enum SuccessSchoolWayMessage {
 
-    SUCCESS_SCHOOL_WAY("등하굣길 등록 성공");
+    SUCCESS_SAVE_SCHOOL_WAY("등하굣길 등록 성공"),
+    SUCCESS_DELETE_SCHOOL_WAY("등하굣길 삭제 성공");
 
     private final String message;
 }
