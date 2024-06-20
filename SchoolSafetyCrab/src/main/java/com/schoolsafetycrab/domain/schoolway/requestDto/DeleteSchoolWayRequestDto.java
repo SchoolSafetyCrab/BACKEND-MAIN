8efd1c35e7cfac5ff5ed14449a6635996821b6fa
honggyeonghyeon <hyeon21owo@gmail.com>
2024-06-20 20:09:51 +0900

    DeleteSchoolWayReqeust dto 생성

---
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/schoolway/requestDto/DeleteSchoolWayRequestDto.java
@@ -0,0 +1,16 @@
+package com.schoolsafetycrab.domain.schoolway.requestDto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class DeleteSchoolWayRequest {
+
+    @NotBlank
+    private long schoolwayId;
+}
