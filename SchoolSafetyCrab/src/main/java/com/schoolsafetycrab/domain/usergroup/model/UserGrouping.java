8a50f9550903706a94433fb2fb1b8684cb4d581f
jaehee Jung <wjfwogml5890@naver.com>
2024-06-20 17:27:47 +0900

    group, user_group table name 변경

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/usergroup/model/UserGrouping.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/usergroup/model/UserGrouping.java
@@ -1,37 +1,36 @@
 package com.schoolsafetycrab.domain.usergroup.model;
 
-import com.schoolsafetycrab.domain.group.model.Group;
+import com.schoolsafetycrab.domain.group.model.Grouping;
 import com.schoolsafetycrab.domain.user.model.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class UserGroup {
+public class UserGrouping {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_group_id")
-    private long userGroupId;
+    @Column(name = "user_grouping_id")
+    private long userGroupingId;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id")
-    private Group group;
+    private Grouping grouping;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    private UserGroup(Group group, User user) {
-        this.group = group;
+    private UserGrouping(Grouping grouping, User user) {
+        this.grouping = grouping;
         this.user = user;
     }
 
-    public static UserGroup createUserGroup(Group group, User user) {
-        return new UserGroup(group, user);
+    public static UserGrouping createUserGroup(Grouping grouping, User user) {
+        return new UserGrouping(grouping, user);
     }
 }
