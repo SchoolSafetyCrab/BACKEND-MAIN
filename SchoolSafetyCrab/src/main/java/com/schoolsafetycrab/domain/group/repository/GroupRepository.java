814ce454ed12679de9a5b70019d907be20bc822c
jaehee Jung <wjfwogml5890@naver.com>
2024-06-21 04:30:50 +0900

    group repository 수정

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/group/repository/GroupRepository.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/group/repository/GroupRepository.java
@@ -4,8 +4,10 @@ import com.schoolsafetycrab.domain.group.model.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
 
-    boolean findByGroupIdAndGroupCode(Long groupId, String groupCode);
+    Optional<Group> findByGroupIdAndGroupCode(Long groupId, String groupCode);
 }
