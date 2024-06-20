133381441f7df7cef71c84f76e558b59f9d6f23f
honggyeonghyeon <hyeon21owo@gmail.com>
2024-06-20 20:21:18 +0900

    SchoolWayRepository deleteBy 쿼리메소드 생성

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/schoolway/repository/SchoolWayRepository.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/schoolway/repository/SchoolWayRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SchoolWayRepository extends JpaRepository<SchoolWay, Long> {
 
-
+    void deleteBySchoolWayId(Long schoolId);
 }
