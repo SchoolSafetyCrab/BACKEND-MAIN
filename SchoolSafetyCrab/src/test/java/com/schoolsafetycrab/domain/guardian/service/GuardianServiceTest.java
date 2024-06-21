20b0fa1b84651556df1cd6db14427d136e2e7886
honggyeonghyeon <hyeon21owo@gmail.com>
2024-06-22 00:49:33 +0900

    GuardianServiceTest 보호자 자녀 길찾기 조회 테스트

--- SchoolSafetyCrab/src/test/java/com/schoolsafetycrab/domain/guardian/service/GuardianServiceTest.java
+++ SchoolSafetyCrab/src/test/java/com/schoolsafetycrab/domain/guardian/service/GuardianServiceTest.java
@@ -2,8 +2,13 @@ package com.schoolsafetycrab.domain.guardian.service;
 
 import com.schoolsafetycrab.domain.guardian.message.responseDto.MyChildrenResponseDto;
 import com.schoolsafetycrab.domain.guardian.repository.GuardianRepository;
+import com.schoolsafetycrab.domain.guardian.requestDto.MyChildrenSchoolWayRequestDto;
+import com.schoolsafetycrab.domain.schoolway.message.responseDto.PointResponseDto;
+import com.schoolsafetycrab.domain.schoolway.model.SchoolWay;
+import com.schoolsafetycrab.domain.schoolwaypoint.model.SchoolWayPoint;
 import com.schoolsafetycrab.domain.user.model.Role;
 import com.schoolsafetycrab.domain.user.model.User;
+import com.schoolsafetycrab.domain.user.repository.UserRepository;
 import com.schoolsafetycrab.global.security.auth.PrincipalDetails;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,6 +36,9 @@ public class GuardianServiceTest {
     private GuardianRepository guardianRepository;
 
     @Mock
+    private UserRepository userRepository;
+
+    @Mock
     private Authentication authentication;
 
     @Mock
@@ -38,6 +46,8 @@ public class GuardianServiceTest {
 
     private User guardian;
     private List<User> children;
+    private MyChildrenSchoolWayRequestDto requestDto;
+    private List<SchoolWayPoint> schoolWayPoints;
 
     @BeforeEach
     public void init(){
@@ -45,6 +55,10 @@ public class GuardianServiceTest {
         children = new ArrayList<>();
         User  user = User.createUser("test","test","test","test", Role.ROLE_STUDENT,"010-1111-1111");
         children.add(user);
+        requestDto = new MyChildrenSchoolWayRequestDto(1);
+        schoolWayPoints = new ArrayList<>();
+        schoolWayPoints.add(SchoolWayPoint.createSchoolWayPoint(SchoolWay.createSchoolWay(user), "1", "2"));
+
     }
 
     @Test
@@ -57,6 +71,23 @@ public class GuardianServiceTest {
 
         //when
         List<MyChildrenResponseDto> responses = guardianService.myChildren(authentication);
+
+        //then
+        Assertions.assertThat(responses.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("보호자 자녀 길찾기 조회 테스트")
+    public void 보호자_자녀_길찾기_조회_테스트(){
+        //given
+        BDDMockito.given(authentication.getPrincipal()).willReturn(principalDetails);
+        BDDMockito.given(principalDetails.getUser()).willReturn(guardian);
+        BDDMockito.given(userRepository.existsUserByUserIdAndState(requestDto.getUserId(), true)).willReturn(true);
+        BDDMockito.given(guardianRepository.existsGuardianByUser_userIdAndId(requestDto.getUserId(), guardian.getId())).willReturn(true);
+        BDDMockito.given(guardianRepository.findSchoolWayByMyChildren(requestDto.getUserId())).willReturn(schoolWayPoints);
+
+        //when
+        List<PointResponseDto> responses = guardianService.findMyChildrenSchoolWay(authentication, requestDto);
 
         //then
         Assertions.assertThat(responses.size()).isEqualTo(1);
