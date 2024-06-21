50a11895bc30387e2fbd00789d23c4d06844b2d7
honggyeonghyeon <hyeon21owo@gmail.com>
2024-06-21 23:06:40 +0900

    GuardianRepository findSchoolWayByMyChildren 생성

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/guardian/repository/GuardianRepository.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/guardian/repository/GuardianRepository.java
@@ -1,6 +1,7 @@
 package com.schoolsafetycrab.domain.guardian.repository;
 
 import com.schoolsafetycrab.domain.guardian.model.Guardian;
+import com.schoolsafetycrab.domain.schoolwaypoint.model.SchoolWayPoint;
 import com.schoolsafetycrab.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,7 @@ public interface GuardianRepository extends JpaRepository<Guardian,Long>, Guardi
 
     @Override
     List<User> findByMyChildren(String id);
+
+    @Override
+    List<SchoolWayPoint> findSchoolWayByMyChildren(long userId);
 }
