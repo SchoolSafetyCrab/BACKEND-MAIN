498f2a49b245fa6fa39ef3fc8a95e5820d3cf9bc
honggyeonghyeon <hyeon21owo@gmail.com>
2024-06-22 00:09:19 +0900

    GuardianController myChildrenSchoolWay 생성

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/guardian/controller/GuardianController.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/guardian/controller/GuardianController.java
@@ -1,12 +1,16 @@
 package com.schoolsafetycrab.domain.guardian.controller;
 
 import com.schoolsafetycrab.domain.guardian.message.responseDto.MyChildrenResponseDto;
+import com.schoolsafetycrab.domain.guardian.requestDto.MyChildrenSchoolWayRequestDto;
 import com.schoolsafetycrab.domain.guardian.service.GuardianService;
+import com.schoolsafetycrab.domain.schoolway.message.responseDto.PointResponseDto;
 import com.schoolsafetycrab.global.util.HttpResponseUtil;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +28,13 @@ public class GuardianController {
     @GetMapping
     public ResponseEntity<?> myChildren(Authentication authentication){
         List<MyChildrenResponseDto> responses = guardianService.myChildren(authentication);
+        ResponseEntity<Map<String,Object>> response = responseUtil.createResponse(responses);
+        return response;
+    }
+
+    @GetMapping("/schoolway")
+    public ResponseEntity<?> myChildrenSchoolWay(Authentication authentication, @Valid @RequestBody MyChildrenSchoolWayRequestDto requestDto){
+        List<PointResponseDto> responses = guardianService.findMyChildrenSchoolWay(authentication, requestDto);
         ResponseEntity<Map<String,Object>> response = responseUtil.createResponse(responses);
         return response;
     }
