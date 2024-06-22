92311a5477d6cba4da85a3331449b1cad357e709
jaehee Jung <wjfwogml5890@naver.com>
2024-06-23 02:24:45 +0900

    사용자 그룹 조회 controller 테스트

---
+++ SchoolSafetyCrab/src/test/java/com/schoolsafetycrab/domain/group/controller/GroupCommonControllerTest.java
@@ -0,0 +1,99 @@
+package com.schoolsafetycrab.domain.group.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.schoolsafetycrab.domain.group.message.SuccessGroupMessage;
+import com.schoolsafetycrab.domain.group.message.responseDto.GroupInfoResponseDto;
+import com.schoolsafetycrab.domain.group.model.Group;
+import com.schoolsafetycrab.domain.group.requestDto.CreateGroupRequestDto;
+import com.schoolsafetycrab.domain.group.requestDto.RegistGroupRequestDto;
+import com.schoolsafetycrab.domain.group.service.GroupService;
+import com.schoolsafetycrab.domain.user.model.Role;
+import com.schoolsafetycrab.domain.user.model.User;
+import com.schoolsafetycrab.global.auth.WithMockAuthUser;
+import com.schoolsafetycrab.global.config.SecurityConfig;
+import com.schoolsafetycrab.global.util.HttpResponseUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebMvcTest(
+        controllers = GroupCommonController.class,
+        excludeFilters = {@ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)}
+)
+@MockBean(JpaMetamodelMappingContext.class)
+public class GroupCommonControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private GroupService groupService;
+
+    @MockBean
+    private HttpResponseUtil responseUtil;
+
+    @Mock
+    private Authentication authentication;
+
+    private User user;
+    private Group group;
+
+    private CreateGroupRequestDto createGroupRequestDto;
+    private List<GroupInfoResponseDto> groupResponse;
+
+    @BeforeEach
+    public void init(){
+
+        createGroupRequestDto = new CreateGroupRequestDto("한밭초등학교", 3, 3, 25, "12345");
+        group = Group.createGroup(createGroupRequestDto);
+
+        groupResponse = new ArrayList<>();
+        groupResponse.add(GroupInfoResponseDto.createGroupInfoResponseDto(group));
+
+    }
+
+    @Test
+    @DisplayName("사용자 그룹 정보 조회 성공 테스트")
+    @WithMockAuthUser(id="test", roles= Role.ROLE_STUDENT)
+    public void 사용자_그룹_정보_조회_성공_테스트() throws Exception{
+        //given
+        Map<String, Object> mockResponseData = new HashMap<>();
+        mockResponseData.put("data", groupResponse);
+
+        BDDMockito.given(groupService.findGroupList(authentication))
+                  .willReturn(groupResponse);
+        BDDMockito.given(responseUtil.createResponse(groupResponse))
+                  .willReturn(ResponseEntity.ok().body(mockResponseData));
+
+        // then
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.get("/api/find/group")
+                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                .contentType(MediaType.APPLICATION_JSON));
+
+        result.andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
