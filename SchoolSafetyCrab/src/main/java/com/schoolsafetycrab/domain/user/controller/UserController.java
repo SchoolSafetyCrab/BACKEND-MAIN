1de75ef9cd09c32d65d1961d2d82b3224037b54e
limjonghyeok <jjognbbang2@naver.com>
2024-06-22 23:36:29 +0900

    컨트롤러서 유저 정보 조회 유저 서비스 연결

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/user/controller/UserController.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/user/controller/UserController.java
@@ -1,11 +1,29 @@
 package com.schoolsafetycrab.domain.user.controller;
 
+import com.schoolsafetycrab.domain.user.message.responseDto.UserInfoResponseDto;
+import com.schoolsafetycrab.domain.user.service.UserService;
+import com.schoolsafetycrab.global.util.HttpResponseUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
+
+    private final UserService userService;
+    private final HttpResponseUtil responseUtil;
+
+    @GetMapping
+    public ResponseEntity<?> findUserInfo(Authentication authentication){
+        UserInfoResponseDto responseDto = userService.findUserInfo(authentication);
+        ResponseEntity<Map<String, Object>> response = responseUtil.createResponse( responseDto);
+        return response;
+    }
 }
