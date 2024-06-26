42ce9f331457cad312b2ac0f7a8ee6094c973a8a
jaehee Jung <wjfwogml5890@naver.com>
2024-06-27 02:55:51 +0900

    UserDevice Entity 생성

---
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/userdevice/model/UserDevice.java
@@ -0,0 +1,34 @@
+package com.schoolsafetycrab.domain.userdevice.model;
+
+import com.schoolsafetycrab.domain.user.model.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class UserDevice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_device_id")
+    private long userDeviceId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @Column(name = "device_token", nullable = false)
+    private String deviceToken;
+
+    private UserDevice(User user, String deviceToken) {
+        this.user = user;
+        this.deviceToken = deviceToken;
+    }
+
+    public static UserDevice createUserDevice(User user, String deviceToken) {
+        return new UserDevice(user, deviceToken);
+    }
+}
