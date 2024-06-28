e79c9306fbe1010b67fdd0b4531277bd23675eac
jaehee Jung <wjfwogml5890@naver.com>
2024-06-29 05:09:16 +0900

    푸시알람 전송 성공 서비스  테스트 작성

--- SchoolSafetyCrab/src/test/java/com/schoolsafetycrab/domain/PushNotification/service/PushNotificationServiceTest.java
+++ SchoolSafetyCrab/src/test/java/com/schoolsafetycrab/domain/PushNotification/service/PushNotificationServiceTest.java
@@ -1,9 +1,14 @@
 package com.schoolsafetycrab.domain.PushNotification.service;
 
+import com.schoolsafetycrab.domain.group.model.Group;
+import com.schoolsafetycrab.domain.group.requestDto.CreateGroupRequestDto;
+import com.schoolsafetycrab.domain.notification.model.Notification;
+import com.schoolsafetycrab.domain.notification.requestDto.CreateNotificationRequestDto;
 import com.schoolsafetycrab.domain.pushnotification.model.PushNotification;
 import com.schoolsafetycrab.domain.pushnotification.repository.PushNotificationRepository;
 import com.schoolsafetycrab.domain.pushnotification.requestDto.SaveDeviceTokenRequestDto;
 import com.schoolsafetycrab.domain.pushnotification.service.PushNotificationService;
+import com.schoolsafetycrab.domain.pushnotification.service.SubjectService;
 import com.schoolsafetycrab.domain.user.model.Role;
 import com.schoolsafetycrab.domain.user.model.User;
 import com.schoolsafetycrab.global.security.auth.PrincipalDetails;
@@ -18,6 +23,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.Authentication;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -27,6 +35,9 @@ public class PushNotificationServiceTest {
 
     @InjectMocks
     private PushNotificationService pushNotificationService;
+
+    @Mock
+    private SubjectService subjectService;
 
     @Mock
     private PushNotificationRepository pushNotificationRepository;
@@ -41,11 +52,20 @@ public class PushNotificationServiceTest {
     private User user;
     private PushNotification pushNotification;
 
+    private Group group;
+    private Notification notification;
+    private List<User> students;
+
     @BeforeEach
     public void init() {
         user = User.createUser("test", "test", "test", "test", Role.ROLE_STUDENT, "010-1234-5678");
         requestDto = new SaveDeviceTokenRequestDto("asdfaweasdfA");
         pushNotification = PushNotification.createUserDevice(user, requestDto);
+
+        group = Group.createGroup(new CreateGroupRequestDto("한밭초등학교", 4, 2, 20, "12345"));
+        notification = Notification.createNotification(group, new CreateNotificationRequestDto(1, "태풍 조심", "태풍 조심해서 등교하기", LocalDate.parse("2024-06-28")));
+        students = new ArrayList<>();
+        students.add(user);
     }
 
     @Test
@@ -85,8 +105,15 @@ public class PushNotificationServiceTest {
     }
 
     @Test
-    @DisplayName("학생들에게 공지 알람 전송")
-    public void 학생들에게_공지_알람_전송_성공_테스트(){
+    @DisplayName("푸시알림 전송 성공 테스트")
+    public void 푸시알림_전송_성공_테스트(){
+        // given
+        BDDMockito.given(subjectService.getGroupStudent(group.getGroupId())).willReturn(students);
 
+        // when
+        Assertions.assertThatNoException().isThrownBy(()->pushNotificationService.pushNotificationToStudents(group.getGroupId(), notification.getNotificationId()));
+
+        // then
+        BDDMockito.then(subjectService).should().getGroupStudent(group.getGroupId());
     }
 }
