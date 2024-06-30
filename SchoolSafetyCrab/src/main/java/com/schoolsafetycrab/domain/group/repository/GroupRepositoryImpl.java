996feac2d360bd111c0a263947e6d93906f8cd7a
limjonghyeok <jjognbbang2@naver.com>
2024-06-30 17:09:04 +0900

    선생님 그룹내 학생 위치 조회 querydsl로 커리 만들기

--- SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/group/repository/GroupRepositoryImpl.java
+++ SchoolSafetyCrab/src/main/java/com/schoolsafetycrab/domain/group/repository/GroupRepositoryImpl.java
@@ -3,6 +3,9 @@ package com.schoolsafetycrab.domain.group.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.schoolsafetycrab.domain.group.model.Group;
 import com.schoolsafetycrab.domain.group.model.QGroup;
+import com.schoolsafetycrab.domain.schoolway.model.QSchoolWay;
+import com.schoolsafetycrab.domain.schoolwaypoint.model.QSchoolWayPoint;
+import com.schoolsafetycrab.domain.schoolwaypoint.model.SchoolWayPoint;
 import lombok.RequiredArgsConstructor;
 
 import java.net.URLEncoder;
@@ -14,6 +17,8 @@ public class GroupRepositoryImpl implements GroupRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     private QGroup group = QGroup.group;
+    private QSchoolWayPoint schoolWayPoint;
+    private QSchoolWay schoolWay;
 
     @Override
     public List<Group> findGroupByKeyword(String keyword)  {
@@ -23,6 +28,16 @@ public class GroupRepositoryImpl implements GroupRepositoryCustom {
                 .from(group)
                 .where(group.schoolName.contains(keyword)
                         .and(group.state.eq(true)))
+                .fetch();
+    }
+
+    @Override
+    public List<SchoolWayPoint> findSchoolWayByStudent(long userId) {
+        return queryFactory
+                .select(schoolWayPoint)
+                .from(schoolWayPoint)
+                .join(schoolWayPoint.schoolWay, schoolWay)
+                .where(schoolWay.user.userId.eq(userId))
                 .fetch();
     }
 }
